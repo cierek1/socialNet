d1255f22e49ca04d6c0dc41388f39be1175da43a
cierek1 <cierek1@wp.pl>
2018-06-05 12:57:37 +0200

    update welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -366,98 +366,99 @@
                                 {{-- <div class="head_har">Posty</div> --}}
     
                                 <div v-for="post,key in posts">
-                                    <div class="col-md-12 col-sm-12 col-xs-12 all_posts" >
-                    
-                                        <div class="col-md-2 pull-left">
-                                            <img :src="'{{url('')}}/public/storage/'+post.user.name+'/' + post.user.pic" alt=""  class="img-circle post_img">                            
-                                        </div>
-                                        <div class="col-md-10">
-                                            <div class="row">
-                                                <div class="col-md-11">
-                                                    <p style="margin-left: -54px;"><a :href="'{{url('profile')}}/' +  post.user.slug" class="post_user_name"> @{{post.user.name}}</a> <br>
-                                                        <span style="color:#AAADB3">  @{{ post.created_at | myOwnTime}}
-                                                        <i class="fa fa-globe"></i></span>
-                                                    </p>
-                                                </div>
-                                                <div class="col-md-1 pull-right">
-                                                    <a href="#" data-toggle="dropdown" aria-haspopup="true"><i class="fa fa-gears" aria-hidden="true"></i></a>
-                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                                                        <li v-if="post.user_id == '{{Auth::user()->id}}'"><a href="#" data-toggle="modal" data-target="#myModal" @click="showUpdatePost(post.id)">Edytuj</a></li>
-                                                        <li><a href="#">Another action</a></li>
-                                                        <li><a href="#">Something else here</a></li>
-                                                        <li role="separator" class="divider"></li>
-                                                        <li v-if="post.user_id == '{{Auth::user()->id}}'"><a @click="deletePost(post.id)"><i class="fa fa-trash-o" aria-hidden="true"> - Usuń</i></a></li>
-                                                    </ul>
-                                                </div>
+
+                                        <div class="col-md-12 col-sm-12 col-xs-12 all_posts" >
+                        
+                                            <div class="col-md-2 pull-left">
+                                                <img :src="'{{url('')}}/public/storage/'+post.user.name+'/' + post.user.pic" alt=""  class="img-circle post_img">                            
                                             </div>
-                                        </div>
-                                        {{-- wyświetlanie zawartości postu   --}}
-                                        <p class="col-md-12" style="color:#000; margin-top:15px; font-family:inherit" >
-                                            @{{post.content}}
-                                            
-                                            <div class="col-md-12" id="article">
-                                                <a :href="post.url" target="_blank">
-                                                    <img :src="post.img" class="img-responsive" alt="" width="620px;">
-                                                    <p>@{{post.title}}</p>
-                                                <span class="description">@{{post.description}}</span>
-                                                <br><br>
-                                                <span class="host">@{{post.host}}</span></a>
-                                            </div>
-                                        </p>
-    
-                                        
-                                        {{-- sekcja lajków --}}
-                                        <div class="col-md-12" style="padding: 10px;" >
-                                            <hr>
-                                           <div class="col-md-4">
-                                                @if(Auth::check())
-        
-                                                {{--  <div v-for="like in likes">
-                                                    <div v-if="post.id == like.post_id && like.user_id == '{{Auth::user()->id}}'">
-                                                        <p class="likeBtn" style="cursor:pointer;"  @click="likePost(post.id)">
-                                                            <i class="fa fa-thumbs-up"> Lubie to</i>
+                                            <div class="col-md-10">
+                                                <div class="row">
+                                                    <div class="col-md-11">
+                                                        <p style="margin-left: -54px;"><a :href="'{{url('profile')}}/' +  post.user.slug" class="post_user_name"> @{{post.user.name}}</a> <br>
+                                                            <span style="color:#AAADB3">  @{{ post.created_at | myOwnTime}}
+                                                            <i class="fa fa-globe"></i></span>
                                                         </p>
                                                     </div>
-                                                </div>  --}}
-                                                
-                                                <p v-if="post.likes.length == 0" @click="likePost(post.id)">
-                                                   <i class="fa fa-thumbs-up" style="cursor:pointer;"> Lubie to</i>                                                    
-                                                </p>
-       
-                                                <p v-else class="likeBtn">
-                                                    <p v-for="like in post.likes" class="likeBtn">
-                                                        <span v-if="like.user_id == '{{Auth::user()->id}}'" @click="deleteLike(post.id)">
-                                                            <i class="fa fa-thumbs-up" style="cursor:pointer;">@{{post.likes.length}}</i>
-                                                        </span>
-                                                        <span v-else @click="likePost(post.id)">
-                                                            <i class="fa fa-thumbs-up" style="cursor:pointer;">@{{post.likes.length}}</i>
-                                                        </span>
-                                                    </p>                                                
-                                                </p> 
-
-                                                @endif
-                                           </div>
-    
-                                            <div class="col-md-4">
-                                                <input type="hidden" v-model="id">
-                                                <p class="showComment" @click="showComment(key)">Komentarze</p>
+                                                    <div class="col-md-1 pull-right">
+                                                        <a href="#" data-toggle="dropdown" aria-haspopup="true"><i class="fa fa-gears" aria-hidden="true"></i></a>
+                                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                                            <li v-if="post.user_id == '{{Auth::user()->id}}'"><a href="#" data-toggle="modal" data-target="#myModal" @click="showUpdatePost(post.id)">Edytuj</a></li>
+                                                            <li><a href="#">Another action</a></li>
+                                                            <li><a href="#">Something else here</a></li>
+                                                            <li role="separator" class="divider"></li>
+                                                            <li v-if="post.user_id == '{{Auth::user()->id}}'"><a @click="deletePost(post.id)"><i class="fa fa-trash-o" aria-hidden="true"> - Usuń</i></a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
                                             </div>
+                                            {{-- wyświetlanie zawartości postu   --}}
+                                            <p class="col-md-12" style="color:#000; margin-top:15px; font-family:inherit" >
+                                                @{{post.content}}
+                                                
+                                                <div class="col-md-12" id="article">
+                                                    <a :href="post.url" target="_blank">
+                                                        <img :src="post.img" class="img-responsive" alt="" width="620px;">
+                                                        <p>@{{post.title}}</p>
+                                                    <span class="description">@{{post.description}}</span>
+                                                    <br><br>
+                                                    <span class="host">@{{post.host}}</span></a>
+                                                </div>
+                                            </p>
+        
+                                            
+                                            {{-- sekcja lajków --}}
+                                            <div class="col-md-12" style="padding: 10px;" >
+                                                <hr>
+                                            <div class="col-md-4">
+                                                    @if(Auth::check())
+            
+                                                    {{--  <div v-for="like in likes">
+                                                        <div v-if="post.id == like.post_id && like.user_id == '{{Auth::user()->id}}'">
+                                                            <p class="likeBtn" style="cursor:pointer;"  @click="likePost(post.id)">
+                                                                <i class="fa fa-thumbs-up"> Lubie to</i>
+                                                            </p>
+                                                        </div>
+                                                    </div>  --}}
+                                                    
+                                                    <p v-if="post.likes.length == 0" @click="likePost(post.id)">
+                                                    <i class="fa fa-thumbs-up" style="cursor:pointer;"> Lubie to</i>                                                    
+                                                    </p>
+        
+                                                    <p v-else class="likeBtn">
+                                                        <p v-for="like in post.likes" class="likeBtn">
+                                                            <span v-if="like.user_id == '{{Auth::user()->id}}'" @click="deleteLike(post.id)">
+                                                                <i class="fa fa-thumbs-up" style="cursor:pointer;">@{{post.likes.length}}</i>
+                                                            </span>
+                                                            <span v-else @click="likePost(post.id)">
+                                                                <i class="fa fa-thumbs-up" style="cursor:pointer;">@{{post.likes.length}}</i>
+                                                            </span>
+                                                        </p>                                                
+                                                    </p> 
+
+                                                    @endif
+                                            </div>
+        
+                                                <div class="col-md-4">
+                                                    <input type="hidden" v-model="id">
+                                                    <p class="showComment" @click="showComment(key)">Komentarze</p>
+                                                </div>
+                                            </div>
+        
                                         </div>
-    
-                                    </div>
-    
-                                    <div class="commentBox" v-if="isShow[key]">
-    
-                                        <div class="comment_form">
-                                            <textarea class="form-control" v-model="commentData[key]"></textarea>
-                                            <button class="btn btn-success" @click="addComment(post,key)">Wyślij</button>
+        
+                                        <div class="commentBox" v-if="isShow[key]">
+        
+                                            <div class="comment_form">
+                                                <textarea class="form-control" v-model="commentData[key]"></textarea>
+                                                <button class="btn btn-success" @click="addComment(post,key)">Wyślij</button>
+                                            </div>
+        
+                                            <ul v-for="comment in post.comments">
+                                                <li>@{{comment.comment}}</li>
+                                            </ul>
                                         </div>
-    
-                                        <ul v-for="comment in post.comments">
-                                            <li>@{{comment.comment}}</li>
-                                        </ul>
-                                    </div>
-    
+
                                 </div>
     
                             </div>
